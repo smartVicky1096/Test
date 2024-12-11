1cc0779444deedf2c2238043abbb52cef356211a
smartVicky1096 <vignesh22892@gmail.com>
2024-12-11 16:49:33 +0530

    contact form verfication

---
+++ com.test/src/test/java/com/test/Test_Contact.java
@@ -0,0 +1,41 @@
+package com.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Test_Contact {
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void launch() {
+		
+	
+	WebDriverManager.edgedriver().setup();
+	driver = new EdgeDriver();
+	driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	driver.manage().window().maximize();
+	
+}
+	@Test
+	public void contact() {
+		driver.get("http://13.201.85.29:3000/");
+		driver.findElement(By.xpath("//input[@name='name']"));
+
+}
+	@AfterMethod
+	public void conclude() {
+		driver.quit();
+		
+	}
+}
+
+
